afcecfc6c6c9121f07fa0586bf818001f3a99f40
Dongmin Kim <ddmkim94@gmail.com>
2024-06-18 15:19:36 +0900

    Test: 뷰 컨트롤러 테스트 작성
    - 뷰 컨트롤러를 생성하고, 기본적인 스펙에 관한 테스트 코드를 작성함.

---
+++ src/main/java/com/fastcampus/projectboardamdin/controller/UserAccountManagementController.java
@@ -0,0 +1,12 @@
+package com.fastcampus.projectboardamdin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/management/user-accounts")
+@RequiredArgsConstructor
+public class UserAccountManagementController {
+
+}
