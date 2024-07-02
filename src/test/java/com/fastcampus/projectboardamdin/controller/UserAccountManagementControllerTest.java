fb02550a6aa75d5b4b552650297076cdf2c7144b
Dongmin Kim <ddmkim94@gmail.com>
2024-07-02 13:25:54 +0900

    Feat: 테스트용 시큐리티 설정 파일 작성
    - 테스트에서 사용할 시큐리티 설정 파일 작성 후 반영

--- src/test/java/com/fastcampus/projectboardamdin/controller/UserAccountManagementControllerTest.java
+++ src/test/java/com/fastcampus/projectboardamdin/controller/UserAccountManagementControllerTest.java
@@ -1,11 +1,10 @@
 package com.fastcampus.projectboardamdin.controller;
 
-import com.fastcampus.projectboardamdin.confiig.SecurityConfig;
+import com.fastcampus.projectboardamdin.config.TestSecurityConfig;
 import com.fastcampus.projectboardamdin.dto.UserAccountDto;
 import com.fastcampus.projectboardamdin.service.UserAccountManagementService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("컨트롤러 - 회원 관리")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(UserAccountManagementController.class) // 슬라이스 테스트
 class UserAccountManagementControllerTest {
 
