2d998b3e56d2d33904fb8ac3c83854ea77c60432
Dongmin Kim <ddmkim94@gmail.com>
2024-07-02 13:27:21 +0900

    Feat: 어드민 회원 컨트롤러 및 테스트 작성

--- src/main/java/com/fastcampus/projectboardamdin/controller/AdminAccountController.java
+++ src/main/java/com/fastcampus/projectboardamdin/controller/AdminAccountController.java
@@ -1,13 +1,13 @@
 package com.fastcampus.projectboardamdin.controller;
 
+import com.fastcampus.projectboardamdin.dto.response.AdminAccountResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/members")
@@ -15,11 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminAccountController {
 
     @GetMapping
-    public String members(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            Model model
-    ) {
+    public String members(Model model) {
 
         return "admin/members";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/admin/members")
+    public List<AdminAccountResponse> getMembers() {
+        return List.of();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    @DeleteMapping("/api/admin/members/{userId}")
+    public void delete(@PathVariable String userId) {
     }
 }
